11e77322cd4ddb0e21ce1bca0c8244f1152f3171
Muhammad Hassan <mehassan3454@gmail.com>
2026-09-07 23:55:42 +0500

    theme endpoint

--- zoanig/urls.py
+++ zoanig/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.profile, name='profile'),
+    path("theme", views.theme, name='theme'),
 ]
--- zoanig/views.py
+++ zoanig/views.py
@@ -6,3 +6,13 @@ def profile(request):
     accept_header = request.META.get('HTTP_ACCEPT', '')
     condition = 'text/html' in accept_header
     return HttpResponse(generate_fetch_layout(DATA, ASCII_ART), content_type="text/plain") if not condition else render(request, 'index.html', {'DATA': DATA, 'ASCII': ASCII_ART})
+
+def theme(request):
+    accept_header = request.META.get('HTTP_ACCEPT', '')
+    condition = 'text/html' in accept_header
+    if not condition:
+        with open('theme.txt', 'r') as file:
+            output = file.read()
+    else:
+        output = 'This endpoint only supports a terminal'
+    return HttpResponse(output, content_type="text/plain")
