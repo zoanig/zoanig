b74d25ec1af3617fba591fe0460c0a31a13ff811
Muhammad Hassan <mehassan3454@gmail.com>
2026-09-13 02:00:47 +0500

    static conf

--- zoanig/settings.py
+++ zoanig/settings.py
@@ -94,6 +94,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 STATIC_URL = "static/"
 
